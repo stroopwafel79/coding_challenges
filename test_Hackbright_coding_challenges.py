9fdbce1f23c01a5950963a5be14d1d45668ee475
stroopwafel79 <stroopwafel79@gmail.com>
2019-01-22 00:50:21 +0000

    Added test_is_anagram_of_palindrome to unittests

--- test_Hackbright_coding_challenges.py
+++ test_Hackbright_coding_challenges.py
@@ -1,11 +1,15 @@
 import unittest
-from Hackbright_coding_challenges import make_dict
+from Hackbright_coding_challenges import make_dict, is_anagram_of_palindrome
 
 
 class TestMakeDict(unittest.TestCase):
 
     def test_make_dict(self):
         self.assertEqual(make_dict('hello'), {"h": 1, "e": 1, "l": 2, "o": 1})
+
+    def test_is_anagram_of_palindrome(self):
+    	self.assertTrue(is_anagram_of_palindrome("arceace"))
+    	self.assertFalse(is_anagram_of_palindrome("arceaceb"))
 
 if __name__ == '__main__':
 
