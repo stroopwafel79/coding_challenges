8e3d0ac77df00d265ce306600d4e7c58d6a2475a
stroopwafel79 <stroopwafel79@gmail.com>
2019-02-05 01:22:48 +0000

    Added handlebars files

---
+++ handlebars.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const handlebars = require('handlebars');
+
+const inFile = './handlebars.hbs';
+const outFile = './handlebars.html';
+
+// load JSON directly using the node.js require function
+const data = require('./handlebars.json');
+
+// retrieve our template from the file
+const source = fs.readFileSync(inFile, 'utf8');
+// compile the template in strict mode
+const template = handlebars.compile(source, { strict: true });
+
+const result = template(data);
+
+console.log(result);
+
+// write the results to an HTML file rather than the console
+fs.writeFileSync(outFile, result);
+console.log(`File written to ${outFile}`);
