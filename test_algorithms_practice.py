31d831e88d5ccd03cdf36e77714c887c62fa18a8
stroopwafel79 <stroopwafel79@gmail.com>
2019-03-08 21:11:35 +0000

    Added test_binary_search unittest

--- test_algorithms_practice.py
+++ test_algorithms_practice.py
@@ -2,4 +2,16 @@ import unittest
 
 from algorithms_practice import binary_search
 
+# Create class with Test infront of what you're going to test
 class TestBinarySearch(unittest.Testcase):
+
+	# create function with test infront of what you're going to test
+	def test_binary_search(self):
+		nums_1 = [i for i in range(25)]
+		nums_2 = [i for i in range(100000)]
+		self.assertFalse(binary_search(50, nums_1))
+		self.assertTrue(binary_search(5, nums_2))
+
+if __name__ == '__main__':
+
+	unittest.main()
