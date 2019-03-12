af73e0ca263942bb6c61fba3eebc6b1e1908bcbe
stroopwafel79 <stroopwafel79@gmail.com>
2019-03-12 23:01:06 +0000

    Added reverse_linked_list

--- Hackbright_coding_challenges.py
+++ Hackbright_coding_challenges.py
@@ -849,6 +849,83 @@ def remove_node(node):
     node.next = node.next.next
 
 
+######## Reverse Linked Lists #########
+
+class Node(object):
+    """Class in a linked list."""
+
+    def __init__(self, data, next=None):
+        self.data = data
+        self.next = next
+
+    def as_string(self):
+        """Represent data for this node and it's successors as a string.
+
+        >>> Node(3).as_string()
+        '3'
+
+        >>> Node(3, Node(2, Node(1))).as_string()
+        '321'
+        """
+
+        out = []
+        n = self
+
+        while n:
+            out.append(str(n.data))
+            n = n.next
+
+        return "".join(out)
+
+def reverse_linked_list(head):
+    """Given LL head node, return head node of new, reversed linked list.
+
+    >>> ll = Node(1, Node(2, Node(3)))
+    >>> new_ll = reverse_linked_list(ll)
+    >>> new_ll.as_string()
+    '321'
+    """
+    # 1 -> 2 -> 3
+
+    # 3 -> 2 -> 1
+
+    ### My solution reverses the LL in place. HB's creates a new LL
+    # current = head
+    # second_curr = current.next
+    # third_curr = current.next.next
+
+    # while second_curr.next:
+    #     if current == head:
+    #         current.next = None
+
+    #     second_curr.next = current
+    #     current = second_curr
+
+    #     if second_curr.next:
+    #         second_curr = third_curr
+
+    #     if second_curr.next:
+    #         third_curr = second_curr.next
+
+    # second_curr.next = current
+    # head = second_curr
+
+    # return head
+
+    ### HB's solution creates a second LL ####
+    ### code is more succinct, but it takes up more memory than mine.
+    ### Not sure which is better
+    out_head = None
+    n = head
+
+    while n:
+        out_head = Node(n.data, out_head)
+        n = n.next
+
+    return out_head
+
+
+
 
 
 if __name__ == "__main__":
