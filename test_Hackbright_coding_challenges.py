6e4b086d9e0c0e7ac7f71887c4ee1c40390153ca
stroopwafel79 <stroopwafel79@gmail.com>
2019-03-14 21:59:14 +0000

    Added unittest methods as a comment

--- test_Hackbright_coding_challenges.py
+++ test_Hackbright_coding_challenges.py
@@ -38,6 +38,20 @@ class TestSumRecursively(unittest.TestCase):
 #     def tearDown(self):
 #         self.widget.dispose()
 
+### Unittest Methods ###
+# assertEqual(a, b)           a == b   
+# assertNotEqual(a, b)        a != b   
+# assertTrue(x)               bool(x) is True  
+# assertFalse(x)              bool(x) is False     
+# assertIs(a, b)              a is b  
+# assertIsNot(a, b)           a is not b  
+# assertIsNone(x)             x is None   
+# assertIsNotNone(x)          x is not None   
+# assertIn(a, b)              a in b  
+# assertNotIn(a, b)           a not in b  
+# assertIsInstance(a, b)      isinstance(a, b)    
+# assertNotIsInstance(a, b)   not isinstance(a, b)
+
 if __name__ == '__main__':
 
     unittest.main()
